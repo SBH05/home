628d8e3cce0fb79cc8f99c89d7457cb882515b20
SBH05 <seobohye5@gmail.com>
2023-10-04 22:09:04 +0900

    초기

---
+++ 01_JAVA_PRACTICE/chap05-array-practice-question/src/main/java/com/ohgiraffers/section01/level01/basic/Application1.java
@@ -0,0 +1,37 @@
+package com.ohgiraffers.section01.level01.basic;
+
+public class Application1 {
+
+    public static void main(String[] args) {
+
+        /* 길이가 10인 정수형 배열을 선언 및 할당한 뒤
+         * 각 인덱스에 차례대로 1부터 10까지 값을 넣고 출력하세요
+         * */
+
+        int[] arry = new int[10];
+        arry[0] = 1;
+        arry[1] = 2;
+        arry[2] = 3;
+        arry[3] = 4;
+        arry[4] = 5;
+        arry[5] = 6;
+        arry[6] = 7;
+        arry[7] = 8;
+        arry[8] = 9;
+        arry[9] = 10;
+
+        System.out.println("arry[0] : " + arry[0]);
+        System.out.println("arry[1] : " + arry[1]);
+        System.out.println("arry[2] : " + arry[2]);
+        System.out.println("arry[3] : " + arry[3]);
+        System.out.println("arry[4] : " + arry[4]);
+        System.out.println("arry[5] : " + arry[5]);
+        System.out.println("arry[6] : " + arry[6]);
+        System.out.println("arry[7] : " + arry[7]);
+        System.out.println("arry[8] : " + arry[8]);
+        System.out.println("arry[9] : " + arry[9]);
+
+
+
+    }
+}
